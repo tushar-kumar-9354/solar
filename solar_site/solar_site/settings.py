433dbcebd7d72f72162b21cfc1354c6dac98516f
tushar-kumar-9354 <jangratushar@348gmail.com>
2025-10-29 23:02:14 +0530

    Add .gitignore to ignore .pyc, venv, and other unnecessary files   0001 allowed

--- solar_site/solar_site/settings.py
+++ solar_site/solar_site/settings.py
@@ -26,7 +26,9 @@ SECRET_KEY = 'django-insecure-u%*5@g4tl!pl!4i=4_7zs6#**vv^pr+bb+4ohgk^yik_b!1$jk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 ALLOWED_HOSTS = ["solar-29lr.onrender.com"]
-
+CSRF_TRUSTED_ORIGINS = [
+    'https://solar-29lr.onrender.com',
+]
 
 # Application definition
 
